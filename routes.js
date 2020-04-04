805089fd9a1284be98b30e498a55612c05428a29
virgiawan267 <virgiawanputra267@gmail.com>
2020-04-04 18:52:15 +0200

    berhasil menampilkan data berdasarkan id

--- routes.js
+++ routes.js
@@ -6,6 +6,9 @@ module.exports = function(app) {
     app.route('/')
         .get(jsonku.index);
 
-        app.route('/tampil')
-            .get(jsonku.tampilsemuamahasiswa);
+    app.route('/tampil')
+        .get(jsonku.tampilsemuamahasiswa);
+    
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
 }
