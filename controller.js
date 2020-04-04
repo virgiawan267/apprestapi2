62a6ed3d0679079562d24fcf1c9f41689f5c0f3a
virgiawan267 <virgiawanputra267@gmail.com>
2020-04-04 17:49:12 +0200

    setting routes di js

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(res,res){
-    response.ok('Aplikasi REST API ku berjalan')
+exports.index = function(req,res){
+    response.ok("Aplikasi REST API ku berjalan",res)
 };
